a8e8fd0d7ad7a07b78f9acd9c0f30ad630ddfb95
Nick Coury <nickcoury@gmail.com>
2016-10-04 20:23:13 -0700

    Switch to alexa-app library. Add gulp file to upload to Lambda.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp   = require('gulp');
+var lambda = require('gulp-awslambda');
+var zip    = require('gulp-zip');
+
+
+/**
+ * For uploading the first time.
+ * Subsequent updates on a function that has already been created only
+ * require the name of the function (see task below).
+ */
+var lambda_params = {
+    FunctionName: 'strava-alexa',
+    Role: '[YOUR LAMBDA EXEC ROLE HERE]'
+};
+
+var opts = {
+    region: 'us-east-1'
+};
+
+gulp.task('lambda', function() {
+    return gulp.src('src/**')
+        .pipe(zip('archive.zip'))
+        //.pipe(lambda(lambda_params, opts))
+        .pipe(lambda(lambda_params.FunctionName, opts))
+        .pipe(gulp.dest('.'));
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-'use strict';
-var StravaSkill = require('./strava-skill');
+var alexa = require('alexa-app');
+var app = new alexa.app('strava');
 
-exports.handler = function (event, context) {
-    var strava = new StravaSkill();
-    strava.process(event, context);
-};
+app.launch(function(request,response) {
+    response.linkAccount();
+    response.say('Welcome to the Strava app. Please link your account');
+});
+
+app.intent('recent',
+    {
+        'slots':{'number':'NUMBER'}
+        ,'utterances':[ 'how many recent activities {1-100|number}' ]
+    },
+    function(request,response) {
+        var number = request.slot('number');
+        response.say('You asked for '+number+' recent activities');
+    }
+);
+
+// Connect to lambda
+exports.handler = app.lambda();
