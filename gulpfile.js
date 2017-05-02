48955cd0d4e0813e537ff9211e6914c7b5d3805f
Nick Coury <nickcoury@gmail.com>
2017-05-01 18:50:42 -0700

    Fix Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var lambda = require('gulp-awslambda');
 var merge = require('merge2');
+var tap = require('gulp-tap');
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 var zip = require('gulp-zip');
@@ -12,20 +13,31 @@ var zip = require('gulp-zip');
  * require the name of the function (see task below).
  */
 var lambda_params = {
-    FunctionName: 'mountain-top-0-2',
-    Role: '[YOUR LAMBDA EXEC ROLE HERE]'
+    FunctionName: 'mountain-top'
 };
 
+if (!process.env.AWS_PROFILE) {
+  console.log('Please provide AWS_PROFILE!');
+  return;
+}
+
 var opts = {
-    region: 'us-east-1'
+    region: 'us-east-1',
+    profile: process.env.AWS_PROFILE
 };
 
 gulp.task('lambda', function() {
-    return merge(
-        tsProject.src().pipe(tsProject()),
-        gulp.src(['src/node_modules/**'], {base: 'src'})
-    ).pipe(zip('archive.zip'))
-        //.pipe(lambda(lambda_params, opts))
-        .pipe(lambda(lambda_params.FunctionName, opts))
-        .pipe(gulp.dest('dist/'));
+  return merge(
+    tsProject.src().pipe(tsProject()),
+    gulp.src(['' +
+    'src/node_modules/**'
+    ], {base: 'src'})
+  ).pipe(tap(function(file) {
+    if (file.isDirectory()) {
+      file.stat.mode = parseInt('40777', 8);
+    }
+  })).pipe(zip('archive.zip'))
+  //.pipe(lambda(lambda_params, opts))
+    .pipe(lambda(lambda_params.FunctionName, opts))
+    .pipe(gulp.dest('dist/'));
 });
